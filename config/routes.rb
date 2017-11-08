acc8c8e225158d574adb6cb300fa1112a12574ed
Chuen Leik Low <chuenleik_3837@hotmail.com>
2017-11-08 18:04:12 +0000

    first

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :venues
+  resources :tweets #Always seven
+  devise_for :users
+  as :user do
+  	get "signin" => 'devise/sessions#new'
+  	delete "signout" => 'devise/sessions#destroy'
+  	get "signup" => 'devise/registrations#new'
+  end
+  root "pages#home"
+  get 'about' => 'pages#about'
+  get 'contact_us' => 'pages#contact-us' # the latter is the FILE_NAME
+  ActiveAdmin.routes(self)
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ db/migrate/20171106121805_create_venues.rb
@@ -0,0 +1,10 @@
+class CreateVenues < ActiveRecord::Migration[5.1]
+  def change
+    create_table :venues do |t|
+      t.string :name
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
