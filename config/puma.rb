0a4b64b09a8de2d1fe31596229200ad1ae022bef
Chuen Leik Low <chuenleik_3837@hotmail.com>
2017-11-16 12:19:05 +0000

    Config changes

--- config/puma.rb
+++ config/puma.rb
@@ -1,11 +1,10 @@
-=begin
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
-=end
 
+=begin
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
@@ -22,3 +21,5 @@ on_worker_boot do
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
+=end
+
