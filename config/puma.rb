9a32ada06c1b730ae378c8cbf6d83b598519e13b
Chuen Leik Low <chuenleik_3837@hotmail.com>
2017-12-07 23:24:12 +0000

    Enabled multithreading

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,11 @@
+=begin
 
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
-=begin
+=end
 
 
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
@@ -23,4 +24,3 @@ on_worker_boot do
   ActiveRecord::Base.establish_connection
 end
 
-=end
