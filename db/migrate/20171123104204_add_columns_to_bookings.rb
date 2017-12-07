01d93df27b46e116f65783269ec38aac63812567
Chuen Leik Low <chuenleik_3837@hotmail.com>
2017-12-07 16:20:33 +0000

    Hopefully fixes migration error

--- db/migrate/20171123104204_add_columns_to_bookings.rb
+++ db/migrate/20171123104204_add_columns_to_bookings.rb
@@ -8,6 +8,8 @@ class AddColumnsToBookings < ActiveRecord::Migration[5.1]
   	add_column :venues, :street_address, :string, :limit => 255
   	add_column :venues, :city, :string, :limit => 255
   	add_column :venues, :nickname, :string, :limit => 255
+    remove_column :bookings, :location_id, :integer
+    add_column :bookings, :venue_id, :integer, :limit => 4
 
   	add_foreign_key :bookings, :clients
   	add_foreign_key :bookings, :venues
