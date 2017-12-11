e909c62fd2616ced733979f16b6f5179193e3097
Chuen Leik Low <chuenleik_3837@hotmail.com>
2017-12-11 14:57:32 +0000

    Stuff

--- app/assets/javascripts/carousel.js
+++ app/assets/javascripts/carousel.js
@@ -28,3 +28,43 @@ document.addEventListener("turbolinks:load", function() {
   });
 });
 
+$(document).ready(function() {
+
+    // page is now ready, initialize the calendar...
+
+    $('#calendar').fullCalendar({
+        events: '/hackyjson/cal/',
+        defaultView: 'basicWeek',
+        header: {
+            left: 'today',
+            center: 'prev title next',
+            right: 'month,basicWeek,agendaDay'
+        },
+        selectable :true,
+        selectHelper: true,
+        editable: true,
+        eventLimit: true,
+        events:[{
+          title: 'All Day Event',
+          start: '2017-12-11'
+        }],
+        select: function(start, end) {
+        $.getScript('/bookings/new', function() {
+          $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
+          date_range_picker();
+          $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
+          $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+          
+        });
+
+        calendar.fullCalendar('unselect');
+        },
+        eventClick:  function(event, jsEvent, view) {
+            $('#modalTitle').html(event.title);
+            $('#modalBody').html(event.description);
+            $('#eventUrl').attr('href',event.url);
+            $('#fullCalModal').modal();
+        }
+    })
+
+});
