7a4fe90d2455e3f12562baeb5f9514486dd8fa78
Chuen Leik Low <chuenleik_3837@hotmail.com>
2018-01-21 13:33:08 +0000

    Added force cascade for one of the migrations due to later conflicts with mmodels

--- db/migrate/20171226134146_create_venues.rb
+++ db/migrate/20171226134146_create_venues.rb
@@ -1,6 +1,6 @@
 class CreateVenues < ActiveRecord::Migration[5.1]
   def change
-    drop_table :venues
+    drop_table :venues, force: :cascade
     create_table :venues do |t|
       t.string :name
       t.integer :price
